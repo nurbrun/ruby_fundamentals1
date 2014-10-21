884471856dedafc9e48efd15ce18c7e3c122b2c8
Bruno Alfano-Rizzo <bruno.alfano.rizzo@gmail.com>
2014-10-21 17:18:17 +0000

    exercise 2, part 2 complete

--- exercise2.rb
+++ exercise2.rb
@@ -4,3 +4,9 @@ meal = 55
 tip = 1.20
 
 puts "a 20 percent tip on a #{meal} dollar meal comes to #{meal*tip} dollars"
+
+
+#exercise 2, part 2
+puts "this is a string"
+number_variable = 5
+puts number_variable.to_s
