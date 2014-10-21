296c5e332b772cc26b6f4666060f1056b302ac48
Bruno Alfano-Rizzo <bruno.alfano.rizzo@gmail.com>
2014-10-21 17:42:58 +0000

    finished the 3rd assignment with birth year calculations

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,13 @@
 puts "what is your name?"
+
+name = gets.chomp
+
+puts "Hi #{name}"
+
+puts "How old are you?"
+
+age = gets.chomp.to_i
+birth_year = 2014-age
+
+puts "Cool! You were born in #{birth_year}"
+
